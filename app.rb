8a93a6e1e3869326a13877cc34a63c5fbb38a9df
Edward Helman <eddhelman@gmail.com>
2016-10-13 15:22:13 -0400

    added capitalization

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require_relative 'random_pair.rb'
 require_relative 'formatter.rb'
 
 get '/' do
-    erb :get_names, :locals => {:name_pairs => name_pairs= " ", :message => " "}
+    erb :get_names, :locals => {:name_pairs => name_pairs= " ", :message => " ", :message2 => "How many people are in are in your class today?"}
 end
 
 post '/names' do
     names = params[:name]
     random_names_array = randomizer(names)
     name_pairs = fix(random_names_array)
-    erb :get_names, :locals => {:name_pairs => name_pairs, :message => "Enter some new names to pair."}
+    erb :get_names, :locals => {:name_pairs => name_pairs, :message => "Enter some new names to pair.", :message2 =>""}
 end
--- formatter.rb
+++ formatter.rb
@@ -2,9 +2,9 @@ def fix(names)
     group = ""
     names.each do |pair|
         if pair.include? (pair[2])
-            group << pair[0] + ", " + pair[1] + ", and " + pair[2] + "<br>"
+            group << pair[0].capitalize + ", " + pair[1].capitalize + ", and " + pair[2].capitalize + "<br>"
         else
-            group << pair[0] + " and " + pair[1] + "<br>"
+            group << pair[0].capitalize + " and " + pair[1].capitalize + "<br>"
         end
     end
     group
